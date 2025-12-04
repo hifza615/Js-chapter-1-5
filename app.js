ca2a10292f21943ba61e020f3acf75e7df416a72
HIFZA EMAAN <hifza.4.106023@gmail.com>
2025-12-04 19:36:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,267 @@
+
+// *****chapter 1*****(question 1)
+//alert("Welcome to my website!");
+
+// *****chapter 1*****(question 2)
+//alert("Welcome to my website!\n");
+
+// *****chapter 1*****(question 3)
+//alert("Welcome to my website!\nHave a nice day!\n");
+
+// *****chapter 1*****(question 4)
+//alert("Hello again!");
+//alert("We are happy to see you.");
+//alert("Thanks for visiting!");
+
+// *****chapter 1*****(question 5)
+//console.log("Hello...I can run js through my web browser's console");
+
+//--_--_--_--_--_--_--_--_//
+
+// ******** Chapter 2 ********
+
+// *****chapter 2*****(question 1)
+//var username;
+
+// *****chapter 2*****(question 2)
+//var myName = "Hifza Emaan";
+
+// *****chapter 2*****(question 3)
+//var message;        // a
+//message = "Hello World";  // b
+//alert(message);     // c
+
+// *****chapter 2*****(question 4)
+//var studentName = "HIFZA EMAAN";
+//var age = 14;
+//var course = "Computer Science";
+//alert("Student Name: " + studentName);
+//alert("Age: " + age);
+//alert("Course: " + course);
+// *****chapter 2*****(question 5)
+//var alertMessage = "Welcome to JavaScript!";
+//alert(alertMessage);
+
+
+// *****chapter 2*****(question 6)
+//var email = "hifza.4.106123@gmail.com";
+//alert("My email address is " + email);
+
+// *****chapter 2*****(question7 )
+//var book = "A smarter way to learn JavaScript";
+//alert("I am trying to learn from the book " + book);
+
+// *****chapter 2*****(question 8)
+//alert("I can write HTML content through JavaScript");
+
+// *****chapter 2*****(question 9)
+//var message2 = "Yah! I can write HTML content through JavaScript";
+//alert(message2);
+//document.write("<h1> YEAH I CAN WRITE HTML CONTENT WITH JAVA SCRIPT</h1>")
+
+//--_--_--_--_--_--_--_--_//
+
+// *****chapter 3*****(question 1)
+//var age = 14;
+//alert("I am " + age + " years old.");
+//alert(you visited this site 1 time);
+
+// *****chapter 3*****(question 2)
+//var visitCount = 1;
+//document.write("You have visited this site " + visitCount + " times.<br>");
+
+// *****chapter 3*****(question 3)
+//var birthYear = 2011;
+//document.write("My birth year is " + birthYear + "<br>");
+//document.write("Data type of my declared variable is number<br>");
+
+// *****chapter 3*****(question 4)
+//var visitorName = "Hifza";
+//var productTitle = "HOODIE";
+//var quantity = 5;
+//document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+//--_--_--_--_--_--_--_--_//
+
+// *****chapter 4*****(question 1)
+//var name, age, city;
+
+// *****chapter 4*****(question 2)
+// Legal variable names
+//var userName, _count, $price, totalMarks, myVar1;
+
+// Illegal variable 
+// var 1name;
+// var my name;
+// var @home;
+// var alert;
+// var my-name;
+
+// *****chapter 4*****(question 3)
+//a)document.write("<hr><h1>Rules for naming JS variables</h1><br><hr>");
+
+//b)document.write("Variable names can only contain letters, numbers, $ and _ . For example $my_1stVariable<br><br>");
+
+//c)document.write("Variables must begin with a letter, $ or _ . For example $name, _name or name<br><br>");
+
+//d)document.write("Variable names are case sensitive<br><br>");
+
+//e)document.write("Variable names should not be JS keywords");
+// ********** CHAPTER 5 **********
+
+// *****chapter 5*****(question 1)
+
+//var num1 = 8;
+//var num2 = 4;
+//var add = num1 + num2;
+
+//document.write("Sum of " + num1 + " and " + num2 + " is " + add + "<br>");
+
+
+// *****chapter 5*****(question 2)
+//var subtract = num1 - num2;
+//var multiply = num1 * num2;
+//var divide = num1 / num2;
+//var mod = num1 % num2;
+
+//document.write("Difference of " + num1 + " and " + num2 + " is " + subtract + "<br>");
+//document.write("Product of " + num1 + " and " + num2 + " is " + multiply + "<br>");
+//document.write("Division of " + num1 + " and " + num2 + " is " + divide + "<br>");
+//document.write("Modulus of " + num1 + " and " + num2 + " is " + mod + "<br><br>");
+
+// *****chapter 5*****(question 3)
+
+// a. 
+//var num;
+
+// b. 
+//document.write("Value after variable declaration is: " + num + "<br>");
+
+// c. 
+//num = 5;
+
+// d. 
+//document.write("Initial value: " + num + "<br>");
+
+// e. 
+//num++;
+
+// f. 
+//document.write("Value after increment is: " + num + "<br>");
+
+// g. 
+//num = num + 7;
+
+// h. 
+//document.write("Value after addition is: " + num + "<br>");
+
+// i. 
+//num--;
+
+// j. 
+//document.write("Value after decrement is: " + num + "<br>");
+
+// k. 
+//var remainder = num % 3;
+
+// l. 
+//document.write("The remainder is: " + remainder);
+
+// *****chapter 5*****(question 4)
+
+//var ticketPrice = 1000;
+//var totalCost = ticketPrice * 5;
+//document.write("Total cost to buy 5 tickets is: " + totalCost + " PKR");
+
+// *****chapter 5*****(question 5)
+//var number = 2;
+
+//document.write("<hr><h2>Table of 4</h2><hr>");
+//document.write("<p>4 *1=4</p>");
+//document.write("<p>4 *2=8</p>");
+//document.write("<p>4 *3=12</p>");
+//document.write("<p>4 *4=16</p>");
+//document.write("<p>4 *5=20</p>");
+//document.write("<p>4 *6=24</p>");
+//document.write("<p>4 *7=28</p>");
+//document.write("<p>4 *8=32</p>");
+//document.write("<p>4 *9=36</p>");
+//document.write("<p>4 *10=40</p>");
+
+//document.write("<hr><h2>Table of 4</h2><hr>");
+// *****chapter 5*****(question 7)
+// Price of items
+// Price of items
+//var priceItem1 = 500;
+//var priceItem2 = 300;
+
+
+//var qtyItem1 = 2;
+//var qtyItem2 = 2;
+//var shippingCharges = 100;
+//var totalItem1 = priceItem1 * qtyItem1;
+//var totalItem2 = priceItem2 * qtyItem2;
+//var totalCost = totalItem1 + totalItem2 + shippingCharges;
+//document.write("Shopping Cart Receipt<br><br>");
+//document.write("Price of item 1: " + priceItem1 + " PKR<br>");
+//document.write("Quantity of item 1: " + qtyItem1 + "<br>");
+//document.write("Price of item 2: " + priceItem2 + " PKR<br>");
+//document.write("Quantity of item 2: " + qtyItem2 + "<br>");
+//document.write("Shipping Charges: " + shippingCharges + " PKR<br><br>");
+//document.write("Total Cost: " + totalCost + " PKR");
+
+// *****chapter 5*****(question 8)
+//let totalMarks = 500; 
+//let marksObtained = 420; 
+//let percentage = (marksObtained / totalMarks) * 100;
+//document.write("Total Marks: " + totalMarks+ 
+      //"\nMarks Obtained: " + marksObtained + 
+     // "\nPercentage: " + percentage + "%");
+  // *****chapter 5*****(question 9)
+     
+//document.write("<hr><h2>CURRENCY IN PKR </h2><hr>");
+//var totalPKR = (10 * 104.80) + (25 * 28);
+    //document.write("Total Currency in PKR: " + totalPKR);
+
+  // *****chapter 5*****(question 10)
+     
+//document.write("Final Result: " + ((8 + 5) * 10 / 2));
+
+
+  // *****chapter 5*****(question 11)
+
+//document.write("<hr><h2>AGE CALCULATOR </h2><hr>");
+//var currentYear = 2025;  
+//var birthYear = 2000;   
+
+//document.write("They are either " + (currentYear - birthYear - 1) + " or " + (currentYear - birthYear) + " years old.");
+
+  // *****chapter 5*****(question 13)
+
+
+//var favoriteSnack = "Chocolate"; 
+//var currentAge = 14;              
+//var maxAge = 80;                 
+//var amountPerDay = 2;             
+
+//var totalNeeded = (maxAge - currentAge) * 365 * amountPerDay;
+
+//document.write("You will need " + totalNeeded + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge + ".");
+
+  // *****chapter 5*****(question 6)
+//var celsius = 30;
+//var fahrenheit = (celsius * 9/5) + 32;
+//document.write(celsius + "°C is " + fahrenheit + "°F<br><br>");
+//var fahrenheit2 = 86;
+//var celsius2 = (fahrenheit2 - 32) * 5/9;
+//document.write(fahrenheit2 + "°F is " + celsius2 + "°C")
+
+  // *****chapter 5*****(question 12)
+//var radius = 8;
+//var pi = 3.142;
+//var circumference = 2 * pi * radius;
+//document.write("The circumference is " + circumference + "<br>");
+//var area = pi * radius * radius;
+//document.write("The area is " + area);
+
+
+//_------THE END---------_//
